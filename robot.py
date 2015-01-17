fbb04f07799e3f09532f45b62d1a6091639a8db7
Phil Fenstermacher <phillip.fenstermacher@gmail.com>
2015-01-17 12:27:06 -0500

    Add relay sample

--- robot.py
+++ robot.py
@@ -18,6 +18,8 @@ class MyRobot(wpilib.IterativeRobot):
         self.drive_train = wpilib.RobotDrive(self.motor_left, self.motor_right)
         self.drive_train.setExpiration(0.2)
 
+        self.light_relay = wpilib.Relay(0, wpilib.Relay.Direction.kForward)
+
     def autonomousInit(self):
         autonomous = True
 
@@ -25,16 +27,22 @@ class MyRobot(wpilib.IterativeRobot):
         self.drive_train.arcadeDrive(self.joystick_right.getY(), self.joystick_left.getX())
 
         # Right Arm Motor
-        if(self.joystick_right.getRawButton(3)):
+        if self.joystick_right.getRawButton(3):
             self.motor_right_arm.set(0.40)
         else:
             self.motor_right_arm.set(0.0)
 
         # Left Arm Motor
-        if(self.joystick_left.getRawButton(3)):
+        if self.joystick_left.getRawButton(3):
             self.motor_left_arm.set(0.40)
         else:
             self.motor_left_arm.set(0.0)
+
+        # Light (turns on with button 1, off with button 2)
+        if self.joystick_right.getRawButton(1):
+            self.light_relay.set(wpilib.Relay.Value.kForward)
+        elif self.joystick_right.getRawButton(2):
+            self.light_relay.set(wpilib.Relay.Value.kOff)
 
 if __name__ == '__main__':
     wpilib.run(MyRobot)
